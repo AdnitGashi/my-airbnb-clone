dbc4de1628eb950de16487a653f6d0ebc752a66a
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2023-01-05 21:33:54 +0100

    initial commit, project setup w tailwind, eslint, next, ts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js, ts,tsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: { extend: {} },
+  plugins: [],
+  variants: {
+    extend: {},
+  },
+};
