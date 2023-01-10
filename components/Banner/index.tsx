7a5c0f16249d4a493bced858c560351e5b84a9ba
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2023-01-10 22:47:47 +0100

    Banner complete??

---
+++ components/Banner/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import MyButton from "../Shared/MyButton";
+
+const Banner = () => {
+  const buttonLabel:string = "I'm flexible"
+  return (
+    <div className="relative h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] 2xl:h-700px">
+      <Image
+        src="/../public/airbnb-banner-picture.png"
+        fill
+        style={{ objectFit: "cover" }}
+        alt="airbnb banner"
+      />
+      <div className="absolute top-1/2 w-full text-center" >
+        <p className="text:sm sm:text-lg" >Not sure where to go? Perfect</p>
+        <MyButton label={buttonLabel} />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- components/Header/MiddleHeader.tsx
+++ components/Header/MiddleHeader.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { MagnifyingGlassIcon  } from '@heroicons/react/20/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
-const MiddleHeader = (props:object)=> {
+const MiddleHeader = (props: object) => {
   return (
     <div className='flex items-center rounded-full md:border-2 py-2 md:shadow-sm' >
-        <input className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 '  type="text" placeholder='Start your search' />
-        <MagnifyingGlassIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 '  />
-       </div>
+      <input className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 ' type="text" placeholder='Start your search' />
+      <MagnifyingGlassIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 ' />
+    </div>
   )
 }
 
